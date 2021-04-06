698fbea6dae17ee4c9528dee54bce29dbbf0ff41
MichalSmyk <michal.smyk38@gmail.com>
2021-04-06 14:54:03 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+eventArr = ['For funeral', 'For work party', 'For wedding', 'For birthday party', 'For Thanksgiving dinner', " For BBQ"];
+clothesArr = ['X-mass jumper', 'bikini', 'bike hemlet', 'pink socks', 'bin bag', 'yellow wig', 'stinky underwear'];
+toDoArr = ['take', 'smell', 'burn', 'sniff', 'destroy'];
+
+
+const randomWord = array => Math.floor(Math.random() * array.length);
+
+function whatWillYouDo()
+ {
+    let randomStuffArr = [];
+
+        randomStuffArr.push(eventArr[randomWord(eventArr)]);
+        randomStuffArr.push(toDoArr[randomWord(toDoArr)]);
+        randomStuffArr.push(clothesArr[randomWord(clothesArr)]);
+
+    let randomStuff = randomStuffArr.join(' ');
+
+    return randomStuff;
+
+};
+
+const check = whatWillYouDo();
+console.log(check);
