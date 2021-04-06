aaafb5e683b6841e7a84561b68738441d943e4cb
MichalSmyk <michal.smyk38@gmail.com>
2021-04-06 15:00:47 +0100

    Add more in ToDo array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 eventArr = ['For funeral', 'For work party', 'For wedding', 'For birthday party', 'For Thanksgiving dinner', " For BBQ"];
 clothesArr = ['X-mass jumper', 'bikini', 'bike hemlet', 'pink socks', 'bin bag', 'yellow wig', 'stinky underwear'];
-toDoArr = ['take', 'smell', 'burn', 'sniff', 'destroy'];
+toDoArr = ['take', 'smell', 'burn', 'sniff', 'destroy', 'wear'];
 
 
 const randomWord = array => Math.floor(Math.random() * array.length);
